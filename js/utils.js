4e7d0e7cd2c488ebc114470450f2e51675deb0ba
DariaYarohovich <dashayarohovich@mail.ru>
2017-02-25 20:02:20 +0300

    Task6: incapsulation

--- js/utils.js
+++ js/utils.js
@@ -1,17 +1,28 @@
-'use strict'
 
-window.utils = {
+(function(exports) {
 
-  getRandomElement: function (arr) {
-    return arr[Math.floor(Math.random()*arr.length)];
-  },
+  var ESCAPE_KEY_CODE = 27;
+  var ENTER_KEY_CODE = 13;
 
-  getRandomElementExcept: function (arr, elem) {
-    var newColor = null;
-
-    while (!newColor || newColor === elem) {
-      newColor = utils.getRandomElement(arr);
-    }
-    return newColor;
+  exports.isActivateEvent = function (event) {
+    return event.keyCode && event.keyCode === ENTER_KEY_CODE;
   }
-}
+
+  exports.isHideEvent = function (event) {
+    return event.keyCode && event.keyCode === ESCAPE_KEY_CODE;
+  }
+
+  var getRandomElement = function (arr) {
+    return arr[Math.floor(Math.random()*arr.length)];
+  };
+
+  exports.getRandomElementExcept = function (arr, elem) {
+      var newColor = null;
+
+      while (!newColor || newColor === elem) {
+        newColor = getRandomElement(arr);
+      }
+      return newColor;
+    }
+
+  })(this.utils = {});
